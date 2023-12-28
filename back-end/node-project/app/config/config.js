02948d920f59b858082453f2812e55e3ccb30655
Muhammad Faisal Ramzan <91006486+Faisalramzan282@users.noreply.github.com>
2023-12-28 09:37:03 +0500

    Update config.js

--- back-end/node-project/app/config/config.js
+++ back-end/node-project/app/config/config.js
@@ -1,9 +1,9 @@
 module.exports = ({
   dialect: 'mysql',
-  username: 'admin', 
-  password: 'admiN@123',
+  username: '', 
+  password: '',
   host: 'localhost',
-  database: 'ecommerce',
+  database: '',
   port: 3306,
   dialectOptions: {
     options: {
@@ -16,6 +16,6 @@ module.exports = ({
     acquire: 30000,
     idle: 10000
   },
-  JWT_SECRET_KEY:'8Zz5tw0Ionm3XPZZfN0NOml3z9FMfmpgXwovR9fp6ryDIoGRM8EPHAB6iHsc0fb',
+  JWT_SECRET_KEY:'',
   saltRounds : 12
 });
